e3dc116876f66b4935b15898b1af0bc64002c6bb
David <38082114+TiggTechTeam@users.noreply.github.com>
2020-04-28 20:45:17 +0200

    Create Pair.java

---
+++ Pair.java
@@ -0,0 +1,16 @@
+public class Pair<T>  {
+
+	private T first, second;
+	
+	public Pair (T one, T two) {
+		first = one;
+		second = two;
+	}
+	public T getFirst() {
+		return first;
+	}
+	public T getSecond() {
+		return second;
+	}
+	
+}
